77efe8509755f55f40089dc28b28616ed3649486
nagendhirankait <nagendhirankait@gmail.com>
2025-04-07 13:35:30 +0530

    init

--- backend/controller/login.controller.ts
+++ backend/controller/login.controller.ts
@@ -7,7 +7,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     const { email, password } = req.body;
 
     try {
-        const user = await User.findOne({ email });
+        const user = await User.findOne({  email: email.trim().toLowerCase() });
         if (!user) {
             res.status(400).json({ result: null, message: 'Invalid credentials' });
             return;
